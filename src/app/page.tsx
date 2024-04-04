6bc7b91af4ec2bef014fe2b4052fbb21ee5ff6f3
Elgun Mehdi <elgun88@gmail.com>
2024-04-04 12:08:02 -0600

    feat: create a homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import FeaturedProducts from './FeaturedProducts/FeaturedProducts';
+import PageHero from './PageHero/PageHero';
+import { ProductCategories, StoreInfo } from '@/components/ui';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <PageHero />
+      <main className='mt-32 grid gap-[clamp(6rem,_12.5vw,_10.5rem)] px-[clamp(1.5rem,_7.5vw,_10rem)] xxl:container'>
+        <ProductCategories />
+        <FeaturedProducts />
+        <StoreInfo />
+      </main>
+    </>
+  );
 }
