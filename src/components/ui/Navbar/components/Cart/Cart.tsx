c8d8a2a12dca9b350e9463f012776d57738f0d05
Elgun Mehdi <elgun88@gmail.com>
2024-05-19 14:53:07 -0600

    refactor: split different parts of Cart into separate components

---
+++ src/components/ui/Navbar/components/Cart/Cart.tsx
@@ -0,0 +1,19 @@
+import { CartButton, CartContent, CartFooter, CartHeader } from './components';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui';
+
+async function Cart() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <CartButton />
+      </DialogTrigger>
+      <DialogContent className='right-[clamp(1.5rem,_7.5vw,_10rem)] top-32 w-[clamp(20rem,_50vw,_23.5rem)] gap-8 rounded-lg py-8 data-[state=closed]:slide-out-to-top-10 data-[state=open]:slide-in-from-top-10'>
+        <CartHeader />
+        <CartContent />
+        <CartFooter />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default Cart;
---
+++ src/components/ui/Navbar/components/Cart/components/index.tsx
@@ -0,0 +1,6 @@
+import CartButton from './CartButton/CartButton';
+import CartContent from './CartContent/CartContent';
+import CartFooter from './CartFooter/CartFooter';
+import CartHeader from './CartHeader/CartHeader';
+
+export { CartButton, CartContent, CartFooter, CartHeader };
