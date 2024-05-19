52ff06792e531efd373607242b54e221f98d68df
Elgun Mehdi <elgun88@gmail.com>
2024-05-19 15:09:50 -0600

    feat: add Cart and Profile components to Navbar

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,6 +1,15 @@
-import { BrandLogo, Cart, NavLinks, NavMenu } from '..';
+import { BrandLogo, NavLinks } from '@/components/ui';
+import { Cart, NavMenu, Profile } from './components';
 
-function Navbar() {
+// import { UserInfo } from '@/types/Users';
+//
+
+type NavbarProps = {
+  isUserSignedIn: boolean;
+  userName: string | undefined | null;
+};
+
+function Navbar({ isUserSignedIn, userName }: NavbarProps) {
   return (
     <nav className='flex items-center justify-between gap-6 py-8 text-background xxl:container md:justify-normal lg:justify-between md:[&>:last-child]:ml-auto lg:[&>:last-child]:ml-0'>
       <NavMenu />
@@ -8,7 +17,10 @@ function Navbar() {
       <ul className='hidden lg:flex lg:gap-9'>
         <NavLinks />
       </ul>
-      <Cart />
+      <div className='flex gap-x-1'>
+        <Cart />
+        <Profile isUserSignedIn={isUserSignedIn} userName={userName} />
+      </div>
     </nav>
   );
 }
