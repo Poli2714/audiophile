15531f02f520c3279abeb5c1919adbf166dc29f3
Elgun Mehdi <elgun88@gmail.com>
2024-05-20 06:38:21 -0600

    refactor: add form components to separate index file

---
+++ src/components/forms/index.tsx
@@ -0,0 +1,36 @@
+import Fieldset from './Fieldset/Fieldset';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './form';
+import { Input } from './input';
+import { RadioGroup, RadioGroupItem } from './radio-group';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './select';
+
+export {
+  Fieldset,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+  RadioGroup,
+  RadioGroupItem,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+};
